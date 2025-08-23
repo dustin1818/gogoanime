69c2ab8181a6eb35fce731027d41d5fd433457b6
dustin1818 <dustinamoda18@gmail.com>
2025-08-23 11:34:58 +0800

    add vue sweet alert package

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import './style.css'
 import App from './App.vue'
 import { router } from './routes'
 import { createPinia } from 'pinia'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(VueSweetalert2);
 app.mount('#app');
 
